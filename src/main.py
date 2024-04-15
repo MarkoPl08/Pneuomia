4dd3b5fe2465f0d368cc600d78e292eea7401011
marko <marko.pleslic@gmail.com>
2024-04-16 00:06:15 +0200

    incorporated the VGG16 architecture

--- src/main.py
+++ src/main.py
@@ -1,103 +1,69 @@
-import numpy as np
-from tensorflow.keras import layers, models, regularizers
+from tensorflow.keras.applications import VGG16
+from tensorflow.keras import layers, models
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
-from tensorflow.keras.callbacks import EarlyStopping, LearningRateScheduler
 from tensorflow.keras.optimizers import Adam
 import matplotlib.pyplot as plt
-import os
 
-model_path = 'my_model.keras'
+baseModel = VGG16(weights="imagenet", include_top=False,
+    input_tensor=layers.Input(shape=(224, 224, 3)))
 
-if os.path.exists(model_path):
-    os.remove(model_path)
-    print(f"Deleted existing model file: {model_path}")
-def adjusted_scheduler(epoch, lr):
-    if epoch < 5:
-        return lr
-    elif epoch < 10:
-        return lr * 0.2
-    else:
-        return lr * 0.02
+for layer in baseModel.layers:
+    layer.trainable = False
 
+headModel = baseModel.output
+headModel = layers.Flatten(name="flatten")(headModel)
+headModel = layers.Dense(256, activation="relu")(headModel)
+headModel = layers.Dropout(0.5)(headModel)
+headModel = layers.Dense(1, activation="sigmoid")(headModel)
 
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    rotation_range=20,
-    width_shift_range=0.2,
-    height_shift_range=0.2,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True,
-    fill_mode='nearest'
-)
+model = models.Model(inputs=baseModel.input, outputs=headModel)
 
-val_datagen = ImageDataGenerator(rescale=1. / 255)
-
-train_generator = train_datagen.flow_from_directory(
-    'data/train',
-    target_size=(150, 150),
-    batch_size=64,
-    class_mode='binary',
-    shuffle=True
-)
-
-validation_generator = val_datagen.flow_from_directory(
-    'data/val',
-    target_size=(150, 150),
-    batch_size=64,
-    class_mode='binary',
-    shuffle=True
-)
-
-model = models.Sequential([
-    layers.Input(shape=(150, 150, 3)),
-    layers.Conv2D(16, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(32, (3, 3), activation='relu'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Flatten(),
-    layers.Dropout(0.5),
-    layers.Dense(256, activation='relu', kernel_regularizer=regularizers.l2(0.001)),
-    layers.Dense(1, activation='sigmoid')
-])
+model.compile(loss="binary_crossentropy", optimizer=Adam(learning_rate=1e-4), metrics=["accuracy"])
 
 
-initial_learning_rate = 1e-4
-model.compile(optimizer=Adam(learning_rate=initial_learning_rate),
-              loss='binary_crossentropy',
-              metrics=['accuracy'])
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   rotation_range=20,
+                                   width_shift_range=0.2,
+                                   height_shift_range=0.2,
+                                   shear_range=0.2,
+                                   zoom_range=0.2,
+                                   horizontal_flip=True,
+                                   fill_mode='nearest')
 
-early_stopping = EarlyStopping(monitor='val_loss', patience=10, verbose=1, restore_best_weights=True)
-lr_schedule = LearningRateScheduler(adjusted_scheduler)
+val_datagen = ImageDataGenerator(rescale=1./255)
 
-history = model.fit(
-    train_generator,
-    steps_per_epoch=train_generator.samples // train_generator.batch_size,
-    epochs=15,
-    validation_data=validation_generator,
-    validation_steps=validation_generator.samples // validation_generator.batch_size,
-    shuffle=True,
-    callbacks=[early_stopping, lr_schedule]
-)
+batch_size = 32
+train_generator = train_datagen.flow_from_directory('data/train',
+                                                    target_size=(224, 224),
+                                                    batch_size=batch_size,
+                                                    class_mode='binary')
+val_generator = val_datagen.flow_from_directory('data/val',
+                                                target_size=(224, 224),
+                                                batch_size=batch_size,
+                                                class_mode='binary')
 
-model.save('my_model.keras')
+history = model.fit(train_generator,
+                    steps_per_epoch=train_generator.samples // batch_size,
+                    validation_data=val_generator,
+                    validation_steps=val_generator.samples // batch_size,
+                    epochs=10)
+
+model.save('vgg16_pneumonia_vs_normal.h5')
 
 plt.plot(history.history['accuracy'])
 plt.plot(history.history['val_accuracy'])
-plt.title('Model accuracy')
+plt.title('Model accuracy with VGG16')
 plt.ylabel('Accuracy')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Validation'], loc='upper left')
-plt.savefig('accuracy_plot.png')
-
+plt.savefig('vgg16_accuracy_plot.png')
 plt.clf()
 
 plt.plot(history.history['loss'])
 plt.plot(history.history['val_loss'])
-plt.title('Model loss')
+plt.title('Model loss with VGG16')
 plt.ylabel('Loss')
 plt.xlabel('Epoch')
 plt.legend(['Train', 'Validation'], loc='upper left')
-plt.savefig('loss_plot.png')
-
+plt.savefig('vgg16_loss_plot.png')
 plt.clf()
